232b360a14f9b9cab8259d99a531d5a375816300
AleksandraTlalka <alektla753@student.polsl.pl>
2020-07-14 19:55:09 +0200

    początek tworzenia dokumentacji

--- KalkulatorONP/Structures.h
+++ KalkulatorONP/Structures.h
@@ -4,12 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * prowizoryczny typ logiczny 
+ */
 typedef enum {
 	false,
 	true
 } bool;
 
-
+/**
+ * Typy znaków wystêpuj¹cych w równaniu
+ */
 enum Type
 {
 	number, dot, begin, end, plus, minus, mulipy, divide, power, percentage, sinus, cosinus, logarithm, negation, clear, done, unn
@@ -20,21 +25,27 @@ enum Type
  */
 struct Onp
 {
-	double value;			///< value of the number
-	enum Type type;			///< type of the operator in the equation
-	struct Onp* pPrev;		///< pointer to the previous unit in the equation
-	struct Onp* pNext;		///< pointer to the next unit in the equation
+	double value;			///< wartoœæ elementu ONP
+	enum Type type;			///< typ znaku w równaniu
+	struct Onp* pPrev;		///< adres poprzedniego elementu równania 
+	struct Onp* pNext;		///< adres nastêpniego elementu równania
 };
 
+/**
+ * Stuktura danych reprezentuj¹ca stos, na którym sk³adowne s¹ wartoœci i znaki przy tworzeniu odwrotnej notacji polskiej
+ */
 struct Stack {
-	double prioryty;
-	enum Type type;
-	struct Stack* pPrev;
+	double prioryty;		///< priorytet elementu na stosie
+	enum Type type;			///< typ znaku na stosie
+	struct Stack* pPrev;	///< adres poprzedniego elementu stosu
 };
 
+/**
+ * Stuktura danych reprezentuj¹ca aktualny stan stosu i przekszta³conego równania
+ */
 struct Pair {
-	struct Stack* pStack;
-	struct Onp* pOnp;
+	struct Stack* pStack;	///< adres g³owy stosu
+	struct Onp* pOnp;		///< adres ogona przekszta³conego równania
 };
 
 #endif STRUCTURE_H
